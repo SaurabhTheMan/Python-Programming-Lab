86fba21c2a7b836c4c812df07a780e0c5dcb8a27
SaurabhTheMan <106165024+SaurabhTheMan@users.noreply.github.com>
2022-05-29 08:47:27 +0530

    Different Methods of String

---
+++ String/Methods Of String.py
@@ -0,0 +1,11 @@
+txt = "I love Valorant, Valorant s my favorite game. Valorant is a Battle Rolyal game. Valorant can be excess on PC. "
+x = txt.count("Valorant")
+print("Count is equal to",x)
+
+txt = "My name is Ståle"
+x = txt.encode()
+print(x)
+
+txt = "nAMASTE, My NamE Is SAURABH"
+x = txt.swapcase()
+print(x)
