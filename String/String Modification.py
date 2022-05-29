4b8e202b7503afd0feffb4ccf25716d7ce9fe4f5
SaurabhTheMan <106165024+SaurabhTheMan@users.noreply.github.com>
2022-05-29 08:10:47 +0530

    Modify String with different way

---
+++ String/String Modification.py
@@ -0,0 +1,15 @@
+#Removing WhiteSpace
+a = " Hello, World! "
+print(a.strip())
+
+#Repalcing
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+
+a = "Hello, World!"
+print(a.split(","))
+
+
+a = "Hello, World!"
+print(a.lower())
